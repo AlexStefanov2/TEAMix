6142ce9ddc1bda0c46245e917067b6204562145f
katapultman <katapultman@abv.bg>
2019-03-15 16:56:59 +0200

    ???

---
+++ Assets/Scripts/MusicControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using Button = UnityEngine.UI.Button;
+
+public class MusicControl : MonoBehaviour
+{
+    public bool isMusicPlaying = true;
+    public Button button;
+    public AudioMixer musicMixer;
+    void Start()
+    {
+        button.OnClick.AddListener(TaskOnClick);
+    }
+    public void Mute()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+    // Update is called once per frame
+    public void Update()
+    {
+        void TaskOnClick()
+        {
+              isMusicPlaying = false;
+              musicMixer.SetFloat("Volume", -80);
+        }
+    }
+}
