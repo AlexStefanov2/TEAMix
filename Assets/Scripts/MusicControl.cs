d47ea730390b8e5519a884326076887bf9af826c
katapultman <katapultman@abv.bg>
2019-03-16 09:30:50 +0200

    Added Working Sprite Swap Sound Button

--- Assets/Scripts/MusicControl.cs
+++ Assets/Scripts/MusicControl.cs
@@ -8,14 +8,24 @@ public class MusicControl : MonoBehaviour
 {
     public bool isMusicPlaying = true;
     public Button musicButton;
+    public Sprite SpriteOriginal;
+    public Sprite SpriteSwap;
+    private int i = 0;
     void Start()
     {
+        SpriteOriginal = musicButton.GetComponent<Image>().sprite;
         musicButton.onClick.AddListener(TaskOnClick);
     }
     // Update is called once per frame
-    void TaskOnClick()
+    public void TaskOnClick()
     {
         AudioListener.pause = !AudioListener.pause;
+        if (i % 2 == 0)
+        {
+            musicButton.image.overrideSprite = SpriteSwap;
+        }
+        else musicButton.image.overrideSprite = SpriteOriginal;
+        i++;
     }
    
 }
