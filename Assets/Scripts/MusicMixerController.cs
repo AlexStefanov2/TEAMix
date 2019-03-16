af89f26d0718528b959cb45437d11bd53054c6bd
katapultman <katapultman@abv.bg>
2019-03-16 12:49:28 +0200

    Added Working Sfx and Music Mute Buttons on Battle Menu

---
+++ Assets/Scripts/MusicMixerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class MusicMixerController : MonoBehaviour
+{
+    public AudioMixer MasterMixer;
+    public Button musicButton;
+    // Start is called before the first frame update
+    void Start()=>musicButton.onClick.AddListener(MusicMute);
+    public void MusicMute()
+    {
+        MasterMixer.SetFloat("MusicVol", -80);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SoundEffectsMixerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SoundEffectsMixerController : MonoBehaviour
+{
+    public AudioMixer MasterMixer; 
+    public bool isSoundEffectPlaying;
+    public Button soundeffectsButton;
+    public Sprite SpriteOriginal;
+    public Sprite SpriteSwap;
+    private int i = 0;
+    // Start is called before the first frame update
+    void Start()=>soundeffectsButton.onClick.AddListener(SoundEffectsMute);
+    public void SoundEffectsMute()
+    {
+        MasterMixer.SetFloat("SfxVol", -80);
+        if (i % 2 == 0)
+        {
+            soundeffectsButton.image.overrideSprite = SpriteSwap;
+        }
+        else soundeffectsButton.image.overrideSprite = SpriteOriginal;
+        i++;
+
+    }
+
+}
