b76c01bb37590ae9129190c2027d948b0c7e234e
katapultman <katapultman@abv.bg>
2019-03-16 11:05:07 +0200

    added prototype settings button

---
+++ Assets/Scripts/MenuPopUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPopUp : MonoBehaviour
+{
+    public GameObject Menupanel;
+    public Button SettingsButton;
+    void Start()
+    {
+        Menupanel.gameObject.SetActive(false);
+        SettingsButton.onClick.AddListener(ShowPanel);
+    }
+
+    // Update is called once per frame
+    public void ShowPanel()
+    {
+        Menupanel.gameObject.SetActive(true);
+    }
+}
