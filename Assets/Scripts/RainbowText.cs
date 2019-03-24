e52d2f0983703d7dc032aebb25900818a03de4d1
Alex Stefanov <alex.stefanov2@gmail.com>
2019-03-24 18:09:20 +0200

    add RainbowText.cs

---
+++ Assets/Scripts/RainbowText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RainbowText : MonoBehaviour
+{
+    public float transitionLength = 3;
+    float t = 0;
+
+    public Text textbox;
+
+    void Update()
+    {
+        t += Time.deltaTime / transitionLength;
+        textbox.color = Color.HSVToRGB(t%1, 1f, 1f);
+    }
+}
